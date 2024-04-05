eb386da668a04516803b2238eef9948eb567ba24
Mangonssen <marc.obst@gmx.net>
2024-04-05 11:29:24 +0200

    Finalized

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello");
+        
+        Dozierender dozierender = new Dozierender("Beispieldozierender","Beispielfakultaet","A01");
+
+        Student student = new Student("Beispielstudent",dozierender);
+
+        student.askQuestion("Sieht man das?","Test",dozierender);
+
+        dozierender.frageBeantworten("Ja.",student,"Test2");   
     }
 }
