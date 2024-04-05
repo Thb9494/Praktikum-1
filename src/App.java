26ce67acd63fdc00f4be5d7cf35451913b75cd91
Theresa Buchner <thb9494@thi.de>
2024-04-05 12:03:58 +0200

    erste Frage

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        Dozierender dozierender = new Dozierender("Beispieldozierender","Beispielfakultaet","A01");
+
+        Student student = new Student("Beispielstudent",dozierender);
+
+        student.askQuestion("Sieht man das?","Test",dozierender);
+
+        dozierender.frageBeantworten("Ja.",student,"Test2"); 
         
     }
 }
